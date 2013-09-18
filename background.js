70860a6b8889859bffce7c3b00a2dca08f9bed04
wysa <wpc009@gmail.com>
2013-09-18 22:54:51 +0800

    alternative method to block ads

--- background.js
+++ background.js
@@ -1,20 +1,47 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+//Copyright wpc009@gmail.chrome
 
-// Sample extension to replace all JPEG images (but no PNG/GIF/... images) with
-// lolcat images from http://icanhascheezburger.com/ - except for images on
-// Google.
+var parseQueryStr = function(query){
+var parts = query.split('&');
+var obj={};
+for(var i in parts){
 
+	var part = parts[i].split('=');
+	if(part.length ==2){
+		obj[part[0]]=part[1];
+	}
+}
+return obj;
+};
 
+var queryObjToString = function (obj){
+	var sep = "";
+	var query="";
+	for(var i in obj){
+		query+=sep;
+		query+=i+'='+obj[i];
+		sep="&";
+	}
+	return query;
+};
+var magicToken = 'ohahaha=1';
 var noAdsLoader = {
   youku: {
-  	reg: /^http[s]?:\/\/static\.youku\.com\/[^?]*[?]?(.*)$/,
-  	url:'https://gitcafe.com/kawaiiushio/antiads/raw/master/loader.swf'
+  	reg: /^http[s]?:\/\/valf\.atm\.youku\.com\/vf\?vip=0(\&.*)$/,
+  	handler:function(p){
+	
+		var obj = parseQueryStr(p);
+		//obj.vl=100;
+		obj.paid=1;
+		console.debug(p);
+		console.debug(obj);
+  		return {redirectUrl:'http://valf.atm.youku.com/vf?'+magicToken+'&vip=1&_t='+ new Date().getTime()+'&'+queryObjToString(obj)};
+  	}
   },
-  tudou: {
-  	url:'https:\//gitcafe.com\/kawaiiushio\/antiads\/raw\/master\/tudou.swf',
-  	reg:/^http[s]?:\/\/js\.tudouui\.com\/[^?]*[?]?(.*)$/
+  sohu :{
+  	reg:/^http[s]?:\/\/v\.aty\.sohu\.com\/v(\?.*)$/,
+  	handler:function(parameters){
+  		return {redirectUrl:'http://wysablog.innoxyz.com/sohu/v'};
+  	}
   }
   //,
   // iqiyi : {
@@ -22,18 +49,18 @@ var noAdsLoader = {
   // 	reg:/^http[s]?:\/\/www\.iqiyi\.com\/[^?]*[?]?(.*)$/
   // }
 }
-
 chrome.webRequest.onBeforeRequest.addListener(
 	function(details){
+		console.log("request: "+details.url+" frameId:"+details.frameId);
 		for(var i in noAdsLoader){
 			var vendor = noAdsLoader[i];
 			var matches = vendor.reg.exec(details.url);
 			if(matches){
-				console.log("hijack "+i+" player");
-				return {redirectUrl:vendor.url};
+				console.log("hijack "+i+" ads ");
+				return vendor.handler(matches[1]);
 			}
 		}
 	},
-	{types:['object'],urls:["*://static.youku.com/**/*.swf","*://js.tudouui.com/**/*.swf"]},
+	{urls:["*://valf.atm.youku.com/vf*","*://v.aty.sohu.com/v*"]},
 	["blocking"]
 	);
